ac11111404e43e722deb736eb59dad46f6f13a37
mrleafaro <91629924+mrleafaro@users.noreply.github.com>
2023-01-26 17:48:36 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 //import { StackNavigator } from '@react-navigation/stack';
-//import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 //import { createStackNavigation } from '@react-navigation/stack';
 //import { createStack } from '@react-navigation/stack';
 
@@ -20,8 +20,8 @@ function App() {
         headerShown: false
       }}>
     
-        {/* <Stack.Screen component="Home" name={HomeScreen} /> */}
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+       
+         <Stack.Screen name="Home" component={HomeScreen}/>
         {/* <Stack.Screen name="Home" component="HomeScreen" /> */}
         {/* <Stack.Screen name:"Home" component:{HomeScreen} /> */}
     
